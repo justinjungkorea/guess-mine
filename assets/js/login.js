1b79379742da25a74ce52f7a932ff7491c3b613e
dongwook jung <jdw8998@daum.net>
2019-09-27 15:48:18 +0900

    #4.1 Login Flow part2

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,11 +7,18 @@ const LOGGED_IN = "loggedIn";
 
 const nickname = localStorage.getItem(NICKNAME);
 
+const logIn = nickname => {
+  const socket = io("/");
+  socket.emit("setNickname", { nickname });
+};
+
 if (nickname === null) {
   body.className = LOGGED_OUT;
 } else {
   body.className = LOGGED_IN;
+  logIn(nickname);
 }
+
 
 const handleFormSubmit = e => {
   e.preventDefault();
@@ -19,6 +26,8 @@ const handleFormSubmit = e => {
   const { value } = input;
   input.value = "";
   localStorage.setItem(NICKNAME, value);
+  body.className = LOGGED_IN;
+  logIn(value);
 };
 
 if (loginForm) {
--- src/socketController.js
+++ src/socketController.js
@@ -1,5 +1,8 @@
 const socketController = socket => {
-    console.log(socket);
-}
+    socket.on("setNickname", ({ nickname }) => {
+        console.log(nickname);
+        socket.nickname = nickname;
+    });
+};
 
 export default socketController;
